b66e6e099c5f19b1284415c4d7bddf390ae750b1
Lucky Leon <leon.weathersby@gmail.com>
2018-03-18 18:30:30 -0500

    "Create read_rename() function in getCrimeData.py"

--- getCrimeData.py
+++ getCrimeData.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-crimeData_2015 = pd.read_excel('Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls', skiprows=3, skipfooter=10, index_col=[0,1])
 
-
-crimeData_2015 = crimeData_2015.rename(columns={'Violent\ncrime':'Violent Crime',
+def read_rename():
+    crimeData_2015 = pd.read_excel('Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls', skiprows=3, skipfooter=10, index_col=[0,1])
+    crimeData_2015 = crimeData_2015.rename(columns={'Violent\ncrime':'Violent Crime',
                                                 'Murder and\nnonnegligent\nmanslaughter':'Murder and NN Manslaughter',
                                                 'Rape\n(revised\ndefinition)1':'Rape newDef',
                                                 'Rape\n(legacy\ndefinition)2':'Rape oldDef',
@@ -12,5 +12,8 @@ crimeData_2015 = crimeData_2015.rename(columns={'Violent\ncrime':'Violent Crime'
                                                 'Larceny-\ntheft':'Larceny',
                                                 'Motor\nvehicle\ntheft':'Motor Vehicle Theft'
                                                 })
+    return crimeData_2015
+
+read_rename()
 
 
